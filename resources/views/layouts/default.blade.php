e2023b4b986f230fbfc27e2f4390ca5c1b2d99af
ly <864777460@qq.com>
2019-03-08 01:23:45 +0000

    初始化样式

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,8 +1,16 @@
 <html>
   <head>
     <title>@yield('title','Sample')</title>
+    <link rel="stylesheet" type="text/css" href="{{URL::asset('css/header.css')}}">
+    <link rel="stylesheet" type="text/css" href="{{URL::asset('css/bootstrap.min.css')}}">
   </head>
   <body>
+  <div class="header">
+  	<div class="title">Weibo App</div>
+  	<div class="login"><a href="">帮助</a><a href="" style="margin-left: 10%;">登录</a></div>
+  </div>
+  <div class="container">
     @yield('content')
+    </div>
   </body>
 </html>
