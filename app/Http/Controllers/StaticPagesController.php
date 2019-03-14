3a390e605a0f36e0497a63ace7ca92a36947bb3b
ly <864777460@qq.com>
2019-03-14 06:35:52 +0000

    首页微博列表

--- app/Http/Controllers/StaticPagesController.php
+++ app/Http/Controllers/StaticPagesController.php
@@ -1,14 +1,19 @@
 <?php
 namespace App\Http\Controllers;
 use Illuminate\Http\Request;
-
+use App\Models\User;
+use Auth;
 /**
 * 
 */
 class StaticPagesController extends Controller
 {
 	public function home(){
-		return view('static_pages/home');
+		if (Auth::check()) {
+	        $statuses = Auth::user()->feed()->paginate(30);
+		}
+		return view('static_pages/home',compact('statuses'));
+
 	}
 
 	public function help(){
